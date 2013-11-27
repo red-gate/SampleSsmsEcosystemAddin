0677b27fbb6833ead26f385aecb023aa0fd98fbb
David Pond <david.pond@red-gate.com>
2013-11-27 19:17:04 +0000

    Intial commit

---
+++ SampleSsmsEcosystemAddin/Extension.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using RedGate.SIPFrameworkShared;
+
+namespace SampleSsmsEcosystemAddin
+{
+    public class Extension : ISsmsAddin
+    {
+        private ISsmsFunctionalityProvider4 m_Provider4;
+        private object m_Dte2;
+
+        public void OnLoad(ISsmsExtendedFunctionalityProvider provider)
+        {
+            m_Provider4 = (ISsmsFunctionalityProvider4) provider;
+            
+            m_Dte2 = m_Provider4.SsmsDte2;
+
+            if(m_Provider4 == null)
+                throw new ArgumentException();
+
+            var subMenus = new SimpleOeMenuItemBase[]
+            {
+                new Menu("Command 1", m_Provider4),
+                new Menu("Command 2", m_Provider4),
+            };
+            
+
+            //m_Provider4.AddToolsMenuItem(new Command());
+
+            m_Provider4.AddGlobalCommand(new SharedCommand(m_Provider4));
+
+            m_Provider4.MenuBar.MainMenu.BeginSubmenu("Sample", "Sample")
+                .BeginSubmenu("Sub 1", "Sub1")
+                .AddCommand("RedGate_Sample_Command")
+                .AddCommand("RedGate_Sample_Command")
+                .EndSubmenu()
+                .BeginSubmenu("Sub 2", "Sub2")
+                //.AddCommand("Command3")
+                //.AddCommand("Command4")
+                .EndSubmenu();
+
+            
+            m_Provider4.AddToolbarItem(new Command());
+
+            m_Provider4.AddTopLevelMenuItem(new Submenu(subMenus));
+        }
+
+        public void OnNodeChanged(ObjectExplorerNodeDescriptorBase node)
+        {
+            //Called when object explorer node selection changes.
+        }
+
+        public string Version { get { return "RedGate.Sample 1.0"; } }
+    }
+}
---
+++ SampleSsmsEcosystemAddin/SharedCommand.cs
@@ -0,0 +1,34 @@
+﻿using RedGate.SIPFrameworkShared;
+
+namespace SampleSsmsEcosystemAddin
+{
+    public class SharedCommand : ISharedCommandWithExecuteParameter
+    {
+        private readonly ISsmsFunctionalityProvider4 m_Provider;
+        private readonly ICommandImage m_CommandImage = new CommandImageNone();
+
+        public SharedCommand(ISsmsFunctionalityProvider4 provider)
+        {
+            m_Provider = provider;
+        }
+
+        public string Name { get { return "RedGate_Sample_Command"; } }
+        public void Execute(object parameter)
+        {
+            m_Provider.QueryWindow.OpenNew("hello");
+        }
+
+        public string Caption { get { return "Red Gate Sample Command"; } }
+        public string Tooltip { get { return "Tooltip"; }}
+        public ICommandImage Icon { get { return m_CommandImage; } }
+        public string[] DefaultBindings { get { return new[] { "global::Ctrl+Alt+D" }; } }
+        public bool Visible { get { return true; } }
+        public bool Enabled { get { return true; } }
+
+
+        public void Execute()
+        {
+            
+        }
+    }
+}
