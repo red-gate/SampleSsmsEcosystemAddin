192eabca40b22ce45cd4e68625c8cc44451df0e6
David Pond <david.pond@red-gate.com>
2015-01-08 14:37:21 +0000

    Create version factory method.

--- SampleSsmsEcosystemAddin/Examples/SsmsFunctionalityProvider/CustomSsmsFunctionProvider.cs
+++ SampleSsmsEcosystemAddin/Examples/SsmsFunctionalityProvider/CustomSsmsFunctionProvider.cs
@@ -3,10 +3,17 @@
     internal class CustomSsmsFunctionProvider : ICustomSsmsFunctionProvider
     {
         private readonly object m_SsmsDte2;
+        private readonly ICustomSsmsFunctionProvider m_VersionSpecificProvider;
 
         public CustomSsmsFunctionProvider(object ssmsDte2)
         {
             m_SsmsDte2 = ssmsDte2;
+            m_VersionSpecificProvider = LoadVersionSpecific(ssmsDte2);
+        }
+
+        private ICustomSsmsFunctionProvider LoadVersionSpecific(object ssmsDte2)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
